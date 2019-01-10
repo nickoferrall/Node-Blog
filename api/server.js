97cbf577c770f95051c375902f0c3097ad88f26e
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-10 12:57:55 -0500

    Added get and put request for post db

--- api/server.js
+++ api/server.js
@@ -3,10 +3,14 @@ const helmet = require('helmet');
 // morgan is a logger - it reads the request and gives you info about it
 const morgan = require('morgan');
 
-// const gatekeeper = require('../gatekeeper/gatekeeperMiddleware.js');
-
+const configureMiddleware = require('../config/middleware');
 const server = express();
 const userDb = require('../data/helpers/userDb');
+const postDb = require('../data/helpers/postDb');
+
+// Config middleware
+
+configureMiddleware(server);
 
 const upperMiddleware = (req, res, next) => {
   console.log('Req body is..', req.body);
@@ -17,17 +21,12 @@ const upperMiddleware = (req, res, next) => {
   next();
 };
 
-// Config middleware
-server.use(express.json());
-server.use(helmet());
-server.use(morgan('dev'));
-
-// server.use(gatekeeper); // using middleware globally. We can also include it before the route handlers (the homies)
-
 // Config endpoints - route handlers are middleware!
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+// Users CRUD
 
 server.get('/api/users/:id', (req, res) => {
   const { id } = req.params;
@@ -37,14 +36,14 @@ server.get('/api/users/:id', (req, res) => {
       if (users.length === 0) {
         res
           .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' });
+          .json({ message: 'The user with the specified ID does not exist.' });
       } else {
         res.status(200).json(users);
       }
     })
     .catch(err => {
       res.status(500).json({
-        error: 'The post information has not been received.'
+        error: 'The user information has not been received.'
       });
     });
 });
@@ -58,7 +57,7 @@ server.get('/api/users/getposts/:id', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        error: 'The post information has not been received.'
+        error: 'The user information has not been received.'
       });
     });
 });
@@ -86,7 +85,7 @@ server.put('/api/users/:id', upperMiddleware, async (req, res) => {
     .catch(err => {
       res
         .status(500)
-        .json({ message: 'The post information could not be modified.' });
+        .json({ message: 'The user information could not be modified.' });
     });
 });
 
@@ -104,6 +103,64 @@ server.delete('/api/users/:id', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ message: 'This user could not be removed.' });
+    });
+});
+
+// Posts CRUD
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'The post information has not been received.'
+      });
+    });
+});
+
+server.get('/api/posts/getTags/:id', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .getPostTags(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'The post information has not been received.'
+      });
+    });
+});
+
+// server.post('/api/posts', async (req, res) => {
+//   try {
+//     const posts = req.body;
+//     const postInfo = await postDb.insert(posts);
+//     res.status(201).json(postInfo);
+//   } catch (error) {
+//     res.status(400).json({
+//       errorMessage: 'There was an error saving the post.'
+//     });
+//   }
+// });
+
+server.put('/api/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  console.log('Changes from put', id);
+  postDb
+    .update(id, changes)
+    .then(count => {
+      res.status(200).json({ message: `${count} post has been updated.` });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'The user information could not be modified.' });
     });
 });
 
