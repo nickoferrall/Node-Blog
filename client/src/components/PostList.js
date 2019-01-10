715546fe410655fc253a55c91805a53d54156815
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-10 17:03:38 -0500

    Added react client with dynamic routing.

---
+++ client/src/components/PostList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PostContainerDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 33%;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  padding: 3%;
+  margin: 3%;
+  width: 100%;
+  background-color: white;
+  border-radius: 2%;
+  text-align: center;
+  cursor: pointer;
+  }
+`;
+
+const TitleDiv = styled.div`
+  width: 100%;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 100;
+  text-decoration: none;
+  margin-bottom: 5%;
+`;
+
+const ContentsDiv = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 100;
+`;
+
+const PostList = props => {
+  console.log('props from post list', props);
+  return (
+    <PostContainerDiv>
+      <StyledLink to={`/post/${props.post.id}`}>
+        <TitleDiv>{props.post.name}</TitleDiv>
+        {/* <ContentsDiv>{props.post.contents}</ContentsDiv> */}
+      </StyledLink>
+    </PostContainerDiv>
+  );
+};
+
+export default PostList;
---
+++ client/src/components/SinglePost.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SinglePostContainerDiv = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
+
+const SinglePostWrapperDiv = styled.div`
+  align-content: center;
+  margin-top: 30%;
+  padding: 5%;
+  background-color: #f2f2f2;
+  border-radius: 2.5%;
+`;
+
+const SinglePostTitle = styled.div`
+  font-weight: 600;
+  padding: 5%;
+`;
+
+const Post = props => {
+  console.log('single post props', props);
+  return (
+    <SinglePostContainerDiv>
+      <SinglePostWrapperDiv>
+        {props.posts.data.map((post, index) => {
+          return post.id === parseInt(props.match.params.id) ? (
+            <div key={index}>
+              <SinglePostTitle>{post.name}</SinglePostTitle>
+              {/* <div>{post.contents}</div> */}
+            </div>
+          ) : null;
+        })}
+      </SinglePostWrapperDiv>
+    </SinglePostContainerDiv>
+  );
+};
+
+export default Post;
